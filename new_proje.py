5a8be27b1c1095f9bc7fb96e414c679e76c176de
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-02 12:07:54 +0300

    türkçe karakterler dikkat eden fonksiyon

---
+++ new_proje.py
@@ -0,0 +1,7 @@
+#Türkçe karakterlere dikkat ederek küçük harfe çeviren fonksiyon
+def formatDomain (domain):
+    turkish_lower_map=  str.maketrans("IİŞĞÜÇÖ","ıişğüçö")
+    return domain.translate(turkish_lower_map).lower()
+
+
+
